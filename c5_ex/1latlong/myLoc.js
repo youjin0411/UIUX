dd4d333413ce95137baeeb4d2b9bf80e86ecc47e
youjin <w2117@e-mirim.hs.kr>
2023-05-18 12:20:27 +0900

    add: gogle map

--- c5_ex/1latlong/myLoc.js
+++ c5_ex/1latlong/myLoc.js
@@ -1,26 +1,41 @@
 /* myLoc.js */
-
-
 window.onload = getMyLocation;
 
 //추가1
-
-
-
-
-
-
-
-
+//브라우저가 페이지를 로드하자마자 getMyLocation 함수를 호출
+function getMyLocation() { 
+    //브라우저가 지오로케이션 API를 지원하는지 여부를 검사하는 부분
+     if (navigator.geolocation) { 
+    //getCurrentPosition메서드를 호출해서 displayLocation이란 핸들러 함수를 전달
+     navigator.geolocation.getCurrentPosition(displayLocation, displayError);
+    //displayLocation 함수는 간단하게 위치를 가져오는 핸드러 함수
+    //errorHandler역시 함수인데 뭔가 문제가 있거나 브라우저가 위치를 결정 못할 때 호출
+     }
+     else {
+     alert("이런, 지오로케이션이 제공되지 않네요");
+     }
+    }
 
 //추가2
-
-
-
-
-
-
-
-
+function displayLocation(position) {//브라우저가 위치정보를 가져올 때 호출
+    var latitude = position.coords.latitude;
+    var longitude = position.coords.longitude;
+   //position.coords객체로부터 위도와 경도 정보를 받는다. var div = document.getElementById("location");
+    var div = document.getElementById("location");
+    div.innerHTML = "당신은 위도: " + latitude + ", 경도: " + longitude + "에 있습니다."; 
+   //innerHTML로 여러분의 위치 정보를 <div>에 설정
+   }
 //추가3
-
+function displayError(error) {//지오로케이션 API로부터 에러를 전달받음
+    //error.code 속성을 사용해서 errorMessage란 새로운 변수에 문자열 중 하나를 할당
+     var errorTypes = {
+    //0에서 3까지 네개의 속성을 가진 객체를 생성, 에러 메세지와 숫자 코드 값을 연결
+     0: "알려지지 않은 에러", 1: "사용자가 권한 거부", 2: "위치를 찾을 수 없음", 3: "요청 응답 시간 초과"
+     };
+     var errorMessage = errorTypes[error.code];
+     if (error.code == 0 || error.code == 2) {
+     errorMessage = errorMessage + " " + error.message;
+     }
+     var div = document.getElementById("location");
+     div.innerHTML = errorMessage; //페이지에 메시지를 추가
+    }
--- c5_ex/2distance/myLoc.js
+++ c5_ex/2distance/myLoc.js
@@ -1,9 +1,9 @@
 /* myLoc.js */
 //추가1
 
-
-
-
+var ourCoords = {
+	latitude: 47.624851, longitude: -122.52099
+   }
 
 window.onload = getMyLocation;
 
@@ -27,7 +27,9 @@ function displayLocation(position) {
 	div.innerHTML = "당신은 위도: " + latitude + ", 경도: " + longitude + "에 있습니다";
 
 //추가2
-
+var distance = document.getElementById("distance");
+ distance.innerHTML = "당신은 WickedlySmart HQ와 " + km + 
+ "km 떨어져 있습니다.";
 
 
 }
@@ -38,7 +40,22 @@ function displayLocation(position) {
 // 구면 코사인 법칙으로 두 위도/경도 지점의 거리를 구함
 //
 //추가3
-
+function computeDistance(startCoords, destCoords) {
+	var startLatRads = degreesToRadians(startCoords.latitude);
+	var startLongRads = degreesToRadians(startCoords.longitude);
+	var destLatRads = degreesToRadians(destCoords.latitude);
+	var destLongRads = degreesToRadians(destCoords.longitude);
+	var Radius = 6371; // radius of the Earth in km(지구의 반경)
+	var distance = Math.acos(Math.sin(startLatRads) * Math.sin(destLatRads) 
+   + 
+	Math.cos(startLatRads) * Math.cos(destLatRads) *
+	Math.cos(startLongRads - destLongRads)) * Radius;
+	return distance;
+   }
+   function degreesToRadians(degrees) { //이 함수는 캔버스에서 자세히 설명
+	radians = (degrees * Math.PI)/180;
+	return radians;
+   }
 
 
 
